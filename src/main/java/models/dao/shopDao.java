38d5de7a99a2aa150b8c7531bd8742cfc0b7b8f9
linusmbae <linusmutugi5178@gmail.com>
2020-05-29 22:40:20 +0300

    shop dao complete

--- src/main/java/models/dao/shopDao.java
+++ src/main/java/models/dao/shopDao.java
@@ -1,11 +1,21 @@
 package models.dao;
 
+import models.CynixShop;
+
+import java.util.List;
+
 public interface shopDao {
 //    create
+    void create(CynixShop cynixShop);
 
 //    read
+    List<CynixShop> getAll();
+    CynixShop getById(int id);
 
 //    update
+    void update(int id,String name, String buildingName, String shopNumber, String email, String phone);
 
 //    destroy
+    void deleteById(int id);
+    void clearAll();
 }
